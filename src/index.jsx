4bbbd93dbce383e9a6d41f2faaa52c6ec3952ebb
Ethan Bloom <109551346+Tang4748@users.noreply.github.com>
2023-09-28 18:18:48 -0500

    changed order of imports on index.jsx
    put bootstrap above index.scss

--- src/index.jsx
+++ src/index.jsx
@@ -1,15 +1,16 @@
 import { createRoot } from "react-dom/client";
 import { MainView } from "./components/main-view/main-view";
 // Import statement to indicate that you need to bundle `./index.scss`
-import "./index.scss";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./index.scss"; 
 
 // Main component (will eventually use all the others)
 const MyFlixApplication = () => {
     return <MainView />;
   };
   
-
+// Find the root of our app
 const container = document.querySelector("#root");
 const root = createRoot(container);
-
+// Tell React to render the app in the root DOM element
 root.render(<MyFlixApplication />);
