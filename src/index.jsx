b7bc5276fdf75709dd079b8dc6e227665a7ce3bf
Ethan Bloom <109551346+Tang4748@users.noreply.github.com>
2023-10-04 18:52:16 -0500

    took out imports for 3.6

--- src/index.jsx
+++ src/index.jsx
@@ -1,16 +1,13 @@
-import { createRoot } from "react-dom/client";
-import { MainView } from "./components/main-view/main-view";
-// Import statement to indicate that you need to bundle `./index.scss`
-import "bootstrap/dist/css/bootstrap.min.css";
-import "./index.scss"; 
+import { createRoot } from 'react-dom/client';
+import { MainView } from './components/main-view/main-view';
 
-// Main component (will eventually use all the others)
+import "./index.scss";
+
 const MyFlixApplication = () => {
     return <MainView />;
-  };
-  
-// Find the root of our app
-const container = document.querySelector("#root");
+};
+
+const container = document.querySelector('#root');
 const root = createRoot(container);
-// Tell React to render the app in the root DOM element
+
 root.render(<MyFlixApplication />);
