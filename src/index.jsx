e5284244e38a6f40a01e6c519a60bfab00b1c15d
Ethan Bloom <109551346+Tang4748@users.noreply.github.com>
2023-10-08 23:03:40 -0500

    added container from bootstrap

--- src/index.jsx
+++ src/index.jsx
@@ -3,12 +3,17 @@ import { MainView } from "./components/main-view/main-view";
 // Import statement to indicate that you need to bundle `./index.scss`
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./index.scss"; 
+import Container from 'react-bootstrap/Container';
 
 // Main component (will eventually use all the others)
 const MyFlixApplication = () => {
-    return <MainView />;
-  };
-  
+  return (
+    <Container style={{border: "1px solid red"}}>
+      <MainView />
+    </Container>
+  )
+};
+
 // Find the root of our app
 const container = document.querySelector("#root");
 const root = createRoot(container);
