22e35ad96bb2148da09b1c997cc6953fa040ca24
Ethan Bloom <109551346+Tang4748@users.noreply.github.com>
2023-10-12 05:15:00 -0500

    minor bugs
    minLenght/maxLength camelCase was incorrect
    signup and login endpoints were wrong
    no need to map movies into state

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -15,13 +15,22 @@ export const MovieCard = ({ movie, onMovieClick }) => { //MovieCard component
 //here is where we define all the props constraints for the MovieCard component
 MovieCard.propTypes = {
     movie: PropTypes.shape({
-        _id: PropTypes.string.isRequired, //id of movie in string format
-        Title: PropTypes.string.isRequired, //title of movie in string format
-        Description: PropTypes.string.isRequired, //description of movie in string format
-        Genre: PropTypes.string.isRequired, //genre of movie in string format [NEEDT TO MAP THIS]
-        Director: PropTypes.string.isRequired, //director of movie in object format
-        ImagePath: PropTypes.string.isRequired, //image path of movie in string format
-        Featured: PropTypes.bool.isRequired //featured status of movie in boolean format
+      _id: PropTypes.string.isRequired, // id of movie in string format
+      Title: PropTypes.string.isRequired, // title of movie in string format
+      Description: PropTypes.string.isRequired, // description of movie in string format
+      Genre: PropTypes.shape({
+        Name: PropTypes.string.isRequired, // name of the genre in string format
+        Description: PropTypes.string.isRequired, // description of the genre in string format
+      }).isRequired,
+      Director: PropTypes.shape({
+        Name: PropTypes.string.isRequired, // name of the director in string format
+        Bio: PropTypes.string.isRequired, // bio of the director in string format
+        Birth: PropTypes.string.isRequired, // birth year of the director in string format
+        Death: PropTypes.string, // death year of the director in string format
+      }).isRequired,
+      Actors: PropTypes.array.isRequired, // an array of actors (you can specify actor PropTypes if needed)
+      ImagePath: PropTypes.string.isRequired, // image path of movie in string format
+      Featured: PropTypes.bool.isRequired, // featured status of movie in boolean format
     }).isRequired,
-    onMovieClick: PropTypes.func.isRequired //onMovieClick prop
-};
+    onMovieClick: PropTypes.func.isRequired, // onMovieClick prop
+  };
