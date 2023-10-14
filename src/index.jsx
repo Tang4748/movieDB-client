71d7ca5fa2546e8a1eccfc21c27e643c59ee23be
Ethan Bloom <109551346+Tang4748@users.noreply.github.com>
2023-10-14 06:50:59 -0500

    changing the order of things so app builds correctly

--- src/index.jsx
+++ src/index.jsx
@@ -1,9 +1,10 @@
 import { createRoot } from "react-dom/client";
 import { MainView } from "./components/main-view/main-view";
 // Import statement to indicate that you need to bundle `./index.scss`
+import { Container } from 'react-bootstrap';
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./index.scss"; 
-import Container from 'react-bootstrap/Container';
+
 
 // Main component (will eventually use all the others)
 const MyFlixApplication = () => {
