6ddbe34c5ead4f6a0c3bc1ef4ed410f9fd890863
Ethan Bloom <109551346+Tang4748@users.noreply.github.com>
2023-11-26 19:51:19 -0600

    Update main-view.jsx

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,105 +1,79 @@
-import { useEffect, useState } from 'react'; //Import statement for useState
-import { MovieCard } from '../movie-card/movie-card'; //Import statement for MovieCard
-import { MovieView } from '../movie-view/movie-view'; //Import statement for MovieView
-import { LoginView } from '../login-view/login-view';//Import statement for LoginView
-import { SignupView } from '../signup-view/signup-view'; //Import statement for SignupView
+import { useEffect, useState } from 'react'; // Import statement for useState
+import { MovieCard } from '../movie-card/movie-card'; // Import statement for MovieCard
+import { MovieView } from '../movie-view/movie-view'; // Import statement for MovieView
+import Row from "react-bootstrap/Row"; // Import statement for Row from react-bootstrap
+import { LoginView } from '../login-view/login-view';
+import { SignupView } from '../signup-view/signup-view';
+import Col from "react-bootstrap/Col"; // Import statement for Col from react-bootstrap
 
 export const MainView = () => { // MainView component
-    const storedUser = JSON.parse(localStorage.getItem('user')); //stores the user in localStorage
-    const storedToken = localStorage.getItem('token'); //stores the token in localStorage
     const [movies, setMovies] = useState([]); //useState hook to store the state of the list of movies
     const [selectedMovie, setSelectedMovie] = useState(null); //useState hook to store the state of the selected movie
-    const [user, setUser] = useState(storedUser? storedUser : null); //useState hook to store the state of the user
-    const [token, setToken] = useState(storedToken? storedToken : null); //useState hook to store the state of the token
+    const [user, setUser] = useState(null); //useState hook to store the state of the user
 
     useEffect(() => { //useEffect hook to retrieve the list of movies when the component is mounted
-        if (!token) //if the token is false, returns
-            return;
-        
-        fetch("https://myflixdb2-49f7e3987c2e.herokuapp.com/movies", { //fetches the list of movies from the API
-            headers: { Authorization: `Bearer ${token}` }//passes the token to the API call
-        })
-            .then((response) => { 
-                if ( response.ok ) { 
-                    return response.json(); //returns the response as JSON if the response is ok
-                }
-                else {
-                    throw Error(response.statusText); //throws an error if the response is not ok
-                }
-            }) //converts the response to JSON format
-            .then((data) => { 
+        fetch('https://myflixdb2-49f7e3987c2e.herokuapp.com/movies') //fetches the list of movies from the API
+            .then((response) => response.json()) //converts the response to JSON format
+            .then((data) => { //stores the data in a state
                 
                 //maps over the list
-                const moviesFromApi = data.map((movies) => { //maps over the list
+                const moviesFromApi = data.map((movie) => { //maps over the list
                 
                 //returns a list of objects with the properties listed below
                 return {
-                    _id: movies._id, //movie._id prop
-                    Title: movies.Title, //movie.Title prop
-                    Description: movies.Description, //movie.Description prop
+                    _id: movie._id, //movie._id prop
+                    Title: movie.Title, //movie.Title prop
+                    Description: movie.Description, //movie.Description prop
                     Genre: {
-                        Name: movies.Genre.Name, //movie.Genre.Name prop
-                        Description: movies.Genre.Description //movie.Genre.Description prop
+                        Name: movie.Genre.Name, //movie.Genre.Name prop
+                        Description: movie.Genre.Description //movie.Genre.Description prop
                     },
                     Director: { //movie.Director prop
-                        Name: movies.Director.Name, //movie.Director.Name prop
-                        Bio: movies.Director.Bio, //movie.Director.Bio prop
+                        Name: movie.Director.Name, //movie.Director.Name prop
+                        Bio: movie.Director.Bio, //movie.Director.Bio prop
                     },
-                    ImagePath: movies.ImagePath, //movie.ImagePath prop
-                    Featured: movies.Featured //movie.Featured prop
+                    ImagePath: movie.ImagePath, //movie.ImagePath prop
+                    Featured: movie.Featured //movie.Featured prop
                 };
-                });
-                
-                setMovies(moviesFromApi); //setMovies prop
+                })
+                setMovies(moviesFromApi); //sets the state of the list of movies
             })
             .catch((error) => { //logs any errors
                 console.log('Error retrieving movies:', error); //logs any errors
             });
-    }, [token]);
+    }, []);
 
-    if (!user) {
-        return (
-            <>
-                <LoginView onLoggedIn={(user, token) => { //If the user is false, returns the LoginView component
-                    setUser(user); //setUser prop
-                    setToken(token); //setToken prop
-                }} />
-                or
+    return (
+        <Row className="justify-content-md-center">
+            {!user ? (
+                <Col md={5}>
+                <LoginView onLoggedIn={(user) => setUser(user)} />
+                <br />
                 <SignupView />
-            </>
-        );
-    }
-
-    if (selectedMovie) {
-        return <MovieView movie={selectedMovie} onBackClick={() => //If the selected movie is true, returns the MovieView component
-            setSelectedMovie(null) //setSelectedMovie prop
-            }
-        />; //If the selected movie is true, returns the MovieView component
-    }
-
-    if (movies.length === 0) { //If the list of movies is empty, returns this message
-        return <div className='main-view'>The list is empty!</div>; //If the list of movies is empty, returns this message
-    }
-    
-    return (//Returns the list of movies
-        <div>
-            <button onClick={() => { //onClick event handler
-                setUser(null); //setUser prop
-                setToken(null); //setToken prop
-                localStorage.clear(); //clears localStorage
-            }}>
-                Logout
-            </button>
-
-            {movies.map((movie) => { //Maps over the list of movies
-                return <MovieCard 
-                key={movie.id} //Key prop
-                movie={movie} //Movie prop
-                onMovieClick={(newSelectedMovie) => { //onMovieClick prop
-                    setSelectedMovie(newSelectedMovie); //setSelectedMovie prop
-                }}
-                />;
-            })}
-        </div>
+                </Col>
+            ) : selectedMovie ? (
+                <Col md={8}>
+                <MovieView
+                    movie={selectedMovie}
+                    onBackClick={() => setSelectedMovie(null)}
+                />
+                </Col>
+            ) : movies.length === 0 ? (
+                <div>The list is empty!</div>
+            ) : (
+                <>
+                    {movies.map((movie) => (
+                        <Col className="mb-5" key={movie.id} md={3}>
+                            <MovieCard
+                                movie={movie}
+                                onMovieClick={(newSelectedMovie) => {
+                                    setSelectedMovie(newSelectedMovie);
+                                }}
+                            />
+                        </Col>
+                    ))}
+                </>
+            )}
+        </Row>
     );
 };
